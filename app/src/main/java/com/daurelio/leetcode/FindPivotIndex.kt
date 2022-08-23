892a97b72832ac4667a00f5bd009e4f185df4e7f
dm4rco <mar.daurelio@gmail.com>
2022-08-23 14:48:09 +0200

    fixed layout + added problem details

--- app/src/main/java/com/daurelio/leetcode/FindPivotIndex.kt
+++ app/src/main/java/com/daurelio/leetcode/FindPivotIndex.kt
@@ -1,6 +1,13 @@
 package com.daurelio.leetcode
 
-import kotlin.time.measureTime
+/*
+* Problem: https://leetcode.com/problems/find-pivot-index/
+*
+* Given an array of integers nums, calculate the pivot index of this array.
+* The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+* If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+* Return the leftmost pivot index. If no such index exists, return -1.
+* */
 
 fun main() {
     val testcase1 = intArrayOf(-1, -1, 1, 1, 0, 0)
@@ -14,12 +21,10 @@ fun main() {
 }
 
 fun pivotIndex(nums: IntArray): Int {
-
-
     var leftSide = 0
     val intSum = nums.sum()
 
-    nums.forEachIndexed { index, element ->
+    nums.forEachIndexed { index, _ ->
         if (leftSide == intSum - leftSide - nums[index]) {
             return index
         }
